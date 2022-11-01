e72cc08cd5c84228d6768d090307b26d21c7af44
FelipeDasr <felipe.heiker07@gmail.com>
2022-11-01 18:37:18 -0400

    feat: function implementations (alterarNotaDeTodasAsRedacoes)

--- libs/repositorios/repositorios.cpp
+++ libs/repositorios/repositorios.cpp
@@ -28,3 +28,49 @@ ResultadosCandidato* obterResultadosPeloCodigoCandidato(
     }
     return NULL;
 }
+
+int alterarNotaDeTodasAsRedacoes(
+    RedacaoVetor* redacoesV,
+    CursoVetor* cursosV,
+    ResultadosCandidatoVetor* resultadosV,
+    CandidatoVetor* candidatosV
+) {
+    // Quantidade de notas que sofreram alterações
+    int notasAlteradas = 0;
+
+    for (int index = 0; index < redacoesV->tamanho; index++) {
+        Redacao* redacao = &redacoesV->redacoes[index];
+
+        // Obtem o candidato que pediu o recurso
+        Candidato* candidato = obterCandidatoPelaInscricao(
+            redacao->inscricaoAluno,
+            candidatosV
+        );
+
+        // Realiza a operação somente se o candidato existir
+        // Já que pode ocorrer de o usuário pedir esse recurso
+        // Antes de ter carregado os arquivos de entrada
+        if (candidato) {
+
+            // Obtem os resultados do candidato
+            ResultadosCandidato* resultados = obterResultadosPeloCodigoCandidato(
+                redacao->inscricaoAluno,
+                resultadosV
+            );
+
+            // Obtem o curso em que o candidato está inscrito
+            Curso* curso = obterCursoPeloCodigo(
+                candidato->codigoCurso,
+                cursosV
+            );
+
+            resultados->redacao = redacao->notaAlterada;
+            notasAlteradas++;
+
+            // Refaz o calculo da nota final
+            calcularNotaFinal(resultados, *curso);
+        }
+    }
+
+    return notasAlteradas;
+}
