8a623e767cec8287dfe14d73a4ee59eefc91d2ba
FelipeDasr <felipe.heiker07@gmail.com>
2022-10-31 20:48:23 -0400

    feat: new function interface (alterarNotaDasRedacoes)

--- libs/repositorios/repositorios.h
+++ libs/repositorios/repositorios.h
@@ -2,3 +2,7 @@
 
 Candidato* obterCandidatoPelaInscricao(int inscricao, CandidatoVetor* candidatosV);
 Curso* obterCursoPeloCodigo(int codigo, CursoVetor* cursosV);
+void alterarNotaDasRedacoes(
+    RedacaoVetor* redacoesV,
+    AcertosVetor* acertosV
+);
