176ce33f6a47d65a7a2ebc3f81bc51e764f7d77f
FelipeDasr <felipe.heiker07@gmail.com>
2022-11-06 12:31:56 -0400

    feat: new interfaces file (ordenacao.h)

---
+++ libs/ordenacao/ordenacao.h
@@ -0,0 +1,6 @@
+#include "registros/registros.h"
+
+void ordenarCandidatosPorNotaECota (CursosComCandidatosVetor* cursos);
+void ordenarCandidatosPorNome(CursosComCandidatosVetor* cursos);
+void ordenarCursosPorOrdemAlfabeticaCrescente(CursosComCandidatosVetor* cursos);
+void ordenarCursosPorOrdemAlfabeticaDecrescente(CursosComCandidatosVetor* cursos);
