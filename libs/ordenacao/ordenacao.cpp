1e8abcff07bdd558d5ff4d68d66e7608f720276b
FelipeDasr <felipe.heiker07@gmail.com>
2022-11-06 23:47:09 -0400

    feat: new functions (compararCotas and obterIndiceDaCota)

--- libs/ordenacao/ordenacao.cpp
+++ libs/ordenacao/ordenacao.cpp
@@ -1,4 +1,5 @@
 #include "ordenacao.h"
+#include <stdio.h>
 #include <string.h>
 
 void ordenarCursosPorOrdemAlfabetica(CursosComCandidatosVetor* cursos) {
@@ -21,4 +22,23 @@ void ordenarCursosPorOrdemAlfabetica(CursosComCandidatosVetor* cursos) {
             }
         }
     }
+}
+
+// Funções utilitárias
+
+int obterIndiceDaCota(char* cota) {
+    char cotas[11][4] = { "AC", "L1", "L3", "L4", "L5", "L7", "L8", "L9", "L11", "L13", "L15" };
+    for (int index = 0; index < 11; index++) {
+        if (strcmp(cota, cotas[index]) == 0) return index;
+    } return -1;
+}
+
+// Caso o retorno seja 1, então cotaA é menor que cotaB
+int compararCotas(char* cotaA, char* cotaB) {
+    int indexCotaA = obterIndiceDaCota(cotaA);
+    int indexCotaB = obterIndiceDaCota(cotaB);
+
+    // Faz a verificação dos índices
+    if (indexCotaA < indexCotaB) return 1;
+    else return 0;
 }
