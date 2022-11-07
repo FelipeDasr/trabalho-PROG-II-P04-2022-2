7b83783ff243d32128ab26920848a1fcd859b471
FelipeDasr <felipe.heiker07@gmail.com>
2022-11-06 21:20:53 -0400

    refactor: update interface name

--- libs/ordenacao/ordenacao.h
+++ libs/ordenacao/ordenacao.h
@@ -2,4 +2,4 @@
 
 void ordenarCandidatosPorNotaECota (CursosComCandidatosVetor* cursos);
 void ordenarCandidatosPorNome(CursosComCandidatosVetor* cursos);
-void ordenarCursosPorOrdemAlfabeticaCrescente(CursosComCandidatosVetor* cursos);
+void ordenarCursosPorOrdemAlfabetica(CursosComCandidatosVetor* cursos);
