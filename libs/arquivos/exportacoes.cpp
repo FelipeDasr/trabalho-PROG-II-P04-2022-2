1a4fc99d677db895e788d8f96e9b753f63902c6c
FelipeDasr <felipe.heiker07@gmail.com>
2022-11-14 00:56:05 -0400

    refactor: update candidate index

--- libs/arquivos/exportacoes.cpp
+++ libs/arquivos/exportacoes.cpp
@@ -1,4 +1,5 @@
 #include "exportacoes.h"
+#include "string.h"
 
 void exportarCandidatosAprovados(
     CursosComCandidatosVetor* cursos, 
@@ -8,12 +9,20 @@ void exportarCandidatosAprovados(
     fprintf(arquivo, "/*LISTA GERAL CLASSIFICADO POR NOTA*/");
 
     for (int indexCurso = 0; indexCurso < cursos->tamanho; indexCurso++) {
-        
+        int colocacaoAtual = 1;
+        char cotaAtual[4];
+
         fprintf(arquivo, "\n\n%d %s\n", 
             cursos->cursos[indexCurso].curso->codigo,
             cursos->cursos[indexCurso].curso->nome
         );
         fprintf(arquivo, "INSC	V_LIN	V_MAT	V_NAT	V_HUM	RED	COTA	NOTA FINAL	CLASSIFICAÇÃO");
+
+        // Pegando a primeira ocorrência da cota para futuras comparações
+        strcpy(
+            cotaAtual,
+            cursos->cursos[indexCurso].informacoesCandidatos[0].candidato->cota
+        );
 
         for (int indexCandidato = 0; indexCandidato < cursos->cursos[indexCurso].tamanho; indexCandidato++) {
             CandidatoInformacoes* candidatoInformacoes = &cursos->cursos[indexCurso].informacoesCandidatos[indexCandidato];
@@ -22,7 +31,14 @@ void exportarCandidatosAprovados(
                 Candidato* candidato = candidatoInformacoes->candidato;
                 ResultadosCandidato* resultados = candidatoInformacoes->resultados;
 
-                fprintf(arquivo, "\n%d\t%.2f\t%.2f\t%.2f\t%.2f\t%d\t%s\t\t%.2f\t\t%d",
+                // Verificando se tem a necessidade de resetar
+                // a colocação e alterar a cota atual
+                if (strcmp(candidato->cota, cotaAtual) != 0) {
+                    strcpy(cotaAtual, candidato->cota);
+                    colocacaoAtual = 1;
+                }
+
+                fprintf(arquivo, "\n%d\t%.2f\t%.2f\t%.2f\t%.2f\t%d\t%s\t\t%.2f\t\t%02d",
                     candidato->inscricao,
                     resultados->linguagens,
                     resultados->matematica,
@@ -31,7 +47,7 @@ void exportarCandidatosAprovados(
                     resultados->redacao,
                     candidato->cota,
                     resultados->notaFinal,
-                    indexCandidato+1
+                    colocacaoAtual++
                 );
             }
         }
